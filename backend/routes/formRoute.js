ccc86c35d87da1d43a6400cb68c9be59ceff1dce
abubaimoro <abubakarimoro27@gmail.com>
2025-07-14 01:27:05 +0000

    separation of concerns

--- backend/routes/formRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { Router } from 'express';
-import { handleFormSubmission } from '../controllers/formControllers.js';
-
-const formRouter = express.Router();
-
-formRouter.post('/submit', handleFormSubmission);
-
-export default formRouter;
